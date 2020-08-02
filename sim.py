550aa8774b79c5150e01ce50ecfca60cb4e51692
Vince Lehman <vslehman@memphis.edu>
2020-08-02 12:39:32 -0500

    Refactor outcome probability calculation

--- sim.py
+++ sim.py
@@ -9,6 +9,8 @@ SECOND_BASE = 1
 THIRD_BASE = 2
 HOME_RUN = 3
 
+DISPLAY_TIME_STEP = 0.5
+
 
 class Game(object):
 
@@ -156,43 +158,43 @@ class Game(object):
         self.stats[self.inning_half]['box_score'][self.inning - 1]['runs'] += 1
         self.runs_per_outcome += 1
 
-    def simulate_plate_appearance(self, batter, pitcher):
-        # Pitcher
-        pitcher_probability = {}
+    def get_pitcher_probabilities(self, pitcher):
         pitching_stats = pitcher.get_pitching_stats()[0]
-        pitcher_probability['walk_pct'] = float(pitching_stats.walks) / pitching_stats.batters_faced
-        pitcher_probability['strikeout_pct'] = float(pitching_stats.strikeouts) / pitching_stats.batters_faced
-        pitcher_probability['hit_pct'] = float(pitching_stats.hits) / pitching_stats.batters_faced
+        pitcher_probabilities = {}
+        pitcher_probabilities['walk_pct'] = float(pitching_stats.walks) / pitching_stats.batters_faced
+        pitcher_probabilities['strikeout_pct'] = float(pitching_stats.strikeouts) / pitching_stats.batters_faced
+        pitcher_probabilities['hit_pct'] = float(pitching_stats.hits) / pitching_stats.batters_faced
+        return pitcher_probabilities
 
-        # Batter
-        batter_probability = {}
+    def get_batter_probabilities(self, batter):
         batting_stats = batter.get_batting_stats()[0]
-        batter_probability['walk_pct'] = float(batting_stats.walks) / batting_stats.at_bats
-        batter_probability['strikeout_pct'] = float(batting_stats.strikeouts) / batting_stats.at_bats
-        batter_probability['hit_pct'] = float(batting_stats.hits) / batting_stats.at_bats
+        batter_probabilities = {}
+        batter_probabilities['walk_pct'] = float(batting_stats.walks) / batting_stats.at_bats
+        batter_probabilities['strikeout_pct'] = float(batting_stats.strikeouts) / batting_stats.at_bats
+        batter_probabilities['hit_pct'] = float(batting_stats.hits) / batting_stats.at_bats
+        return batter_probabilities
 
+    def average_and_calculate_outcome(self, pitcher_stats, batter_stats, stat):
+        rng = random.random()
+        avg_prob = (pitcher_stats[stat] + batter_stats[stat]) / 2
+        if rng < avg_prob:
+            return True
+        return False
+
+
+    def simulate_plate_appearance(self, batter, pitcher):
+        pitcher_probability = self.get_pitcher_probabilities(pitcher)
+        batter_probability = self.get_batter_probabilities(batter)
+
+        simulations = [
+            ('walk_pct', self.walk),
+            ('strikeout_pct', self.strikeout),
+            ('hit_pct', self.hit),
+        ]
         possible_outcomes = []
-
-        # Walk
-        rng = random.random()
-        avg_prob = (batter_probability['walk_pct'] + pitcher_probability['walk_pct']) / 2
-        # print('walk', avg_prob)
-        if rng < avg_prob:
-            possible_outcomes.append(self.walk)
-
-        # Strikeout
-        rng = random.random()
-        avg_prob = (batter_probability['strikeout_pct'] + pitcher_probability['strikeout_pct']) / 2
-        # print('strikeout', avg_prob)
-        if rng < avg_prob:
-            possible_outcomes.append(self.strikeout)
-
-        # Hit
-        rng = random.random()
-        avg_prob = (batter_probability['hit_pct'] + pitcher_probability['hit_pct']) / 2
-        # print('hit', avg_prob)
-        if rng < avg_prob:
-            possible_outcomes.append(self.hit)
+        for stat, sim_callback in simulations:
+            if self.average_and_calculate_outcome(pitcher_probability, batter_probability, stat):
+                possible_outcomes.append(sim_callback)
 
         if possible_outcomes:
             outcome = random.choice(possible_outcomes)
@@ -227,7 +229,7 @@ class Game(object):
                 hitter,
                 pitcher,
             )
-            time.sleep(0.5)
+            time.sleep(DISPLAY_TIME_STEP)
 
     def simulate(self):
         self.start_game()
@@ -277,7 +279,12 @@ class Game(object):
         print('\n{} win!\n'.format(winning_team))
 
         num_innings = len(self.stats['away']['box_score']) + 1
-        print('Team    1    2    3    4    5    6    7    8    9{}    R    H    E'.format(''.join(['{:>5}'.format(i) for i in range(10, num_innings)])))
+        box_score_header = 'Team    1    2    3    4    5    6    7    8    9'
+        if num_innings > 9:
+            for i in range(10, num_innings):
+                box_score_header += '{:>5}'.format(i)
+        box_score_header += '    R    H    E'
+        print(box_score_header)
         self._print_team_line(self.away_team.teamID, 'away')
         self._print_team_line(self.home_team.teamID, 'home')
 
@@ -306,7 +313,6 @@ if __name__ == '__main__':
         action='store',
     )
     args = parser.parse_args()
-
 
     home = models.Teams.filter(
         yearId=args.home_year,
